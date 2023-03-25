4c4217ab6802a00c846bd1617d81f01bccab3a20
sky_tmp <sky_tmp@outlook.com>
2023-03-25 15:54:23 +0800

    将 url 和 appid 挪到配置文件里

--- src/main/java/com/example/servingwebcontent/controller/AuthorizeController.java
+++ src/main/java/com/example/servingwebcontent/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.servingwebcontent.dto.AccessTokenDTO;
 import com.example.servingwebcontent.dto.GithubUser;
 import com.example.servingwebcontent.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
@@ -13,14 +14,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback( @RequestParam(name = "code") String code,
                             @RequestParam(name = "state") String state){
+
+
+
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("9bb3b639b3ed31956b98");
-        accessTokenDTO.setClient_secret("26a5b73d3da1f0eda80b467afed193989f4390da");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
