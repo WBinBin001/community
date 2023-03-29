45f7bdd4c931460d3f31c555041935118c7d1265
sky_tmp <sky_tmp@outlook.com>
2023-03-29 21:59:12 +0800

    链接 h2 数据库，并插入 登陆信息

--- src/main/java/com/example/servingwebcontent/controller/AuthorizeController.java
+++ src/main/java/com/example/servingwebcontent/controller/AuthorizeController.java
@@ -1,13 +1,20 @@
 package com.example.servingwebcontent.controller;
 import com.example.servingwebcontent.dto.AccessTokenDTO;
 import com.example.servingwebcontent.dto.GithubUser;
+import com.example.servingwebcontent.model.User;
 import com.example.servingwebcontent.provider.GithubProvider;
+import com.example.servingwebcontent.mapper.UserMapper;
+
+
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
 
 @Controller
 public class AuthorizeController {
@@ -21,12 +28,17 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+
+    private final UserMapper userMapper;
+
+    public AuthorizeController(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     @GetMapping("/callback")
-    public String callback( @RequestParam(name = "code") String code,
-                            @RequestParam(name = "state") String state,
-                            HttpServletRequest request){
-
-
+    public String callback(@RequestParam(name = "code") String code,
+                         @RequestParam(name = "state") String state,
+                         HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -36,10 +48,19 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+
+
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             // 登录成功，写cookie 和session
-            request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         } else {
             // 登录失败，重新登录
---
+++ src/main/java/com/example/servingwebcontent/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.servingwebcontent.mapper;
+import com.example.servingwebcontent.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Mapper
+public interface UserMapper {
+//    @Insert("create table public.user_03( id INT AUTO_INCREMENT primary key , account_id varchar, name varchar, token varchar, gmt_creat bigint, gmt_modified bigint)")
+
+    @Insert("insert into user01 (name,account_id,token,gmt_creat,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
